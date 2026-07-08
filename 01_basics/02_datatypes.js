dd216c8d77b197ff87e915b3040706b34c37e79f
Shivam <shivamforsure2006@gmail.com>
2026-07-08 11:30:48 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treal all JS code as newer version
+
+
+
+// datatypes
+
+let name = "Shivam"    // string
+
+let age = 20           // integer
+
+let isLoggedin = false // boolean
+
+
+console.log(typeof "Shivam")      // string
+
+console.log(typeof age) //number
+
+console.log(typeof null) // object
+
+console.log(typeof undefined)     // undefined
