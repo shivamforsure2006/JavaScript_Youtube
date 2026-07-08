be7d040ee63f59704369dff7c7dbb5171c2b415e
Shivam <shivamforsure2006@gmail.com>
2026-07-08 11:16:56 +0000

    let, const and var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 7011
+
+let accountEmail = "shivam@gmail.com"
+
+console.log(accountId);
+
+console.table([accountEmail , accountId])
+
+
+/*
+prefer not to use var instead always use let and const
+
+wihtout declaring this i can also decalre varibles
+
+accountCity = "Delhi"
+
+*/
